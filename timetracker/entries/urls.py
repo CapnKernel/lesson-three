5d6a9bfc55f4c561d149c485cbdb5f1e2ff79ae0
Mitch Davis <mjd@hackvana.com>
2015-08-03 20:42:15 +1000

    Ok, here's my homework
    Has four views/pages, and nav bar shows active page.  The way the
    nav bar active stuff is done is a bit hacky, but it works.

--- timetracker/entries/urls.py
+++ timetracker/entries/urls.py
@@ -4,5 +4,8 @@ from entries import views
 
 
 urlpatterns = [
-    url(r'^entries/$', views.entries, name='entries'),
+    url(r'^entries/?$', views.entries, name='entries'),
+    url(r'^clients/?$', views.clients, name='clients'),
+    url(r'^clients/(?P<id>[^/]+)/?$', views.client_summary, name='client_summary'),
+    url(r'^projects/?$', views.projects, name='projects'),
 ]
--- timetracker/entries/views.py
+++ timetracker/entries/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Entry
+from .models import Entry, Client, Project
 
 
 def entries(request):
     entry_list = Entry.objects.all()
     return render(request, 'entries.html', {
         'entry_list': entry_list,
+    })
+
+def projects(request):
+    project_list = Project.objects.all()
+    return render(request, 'projects.html', {
+        'project_list': project_list,
+    })
+
+def clients(request):
+    client_list = Client.objects.all()
+    return render(request, 'clients.html', {
+        'client_list': client_list,
+    })
+
+def client_summary(request, id):
+    client = get_object_or_404(Client, pk=id)
+
+    # This could also be done in the template with {{ client.project_set }}
+    projects_list = Project.objects.filter(client=client)
+
+    return render(request, 'client_summary.html', {
+        'client': client,
+        'projects_list': projects_list,
     })
